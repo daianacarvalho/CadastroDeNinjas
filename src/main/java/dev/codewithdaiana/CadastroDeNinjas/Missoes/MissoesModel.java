71e9cb6c59b14590e1104def6cef033f55a6dc64
Daiana <carvalhodaiana42@gmail.com>
2025-12-24 17:02:03 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/dev/codewithdaiana/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/codewithdaiana/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.codewithdaiana.CadastroDeNinjas.Missoes;
 
 import dev.codewithdaiana.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
