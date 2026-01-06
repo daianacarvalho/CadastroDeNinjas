91693f1615d1f35cfb61553b4b756d419ea01b57
Daiana <carvalhodaiana42@gmail.com>
2026-01-06 17:13:19 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/codewithdaiana/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/codewithdaiana/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.codewithdaiana.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,4 +10,34 @@ public class NinjaController {
         return "Essa é minha primeira mensagem nessa rota";
     }
 
-}
+    // Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar Ninja";
+    }
+
+    // Mostrar ninja  por id (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId() {
+        return "Mostrar Ninja por id";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar Ninja por id";
+    }
+
+    // Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Ninja deletado por id";
+
+
+    }
